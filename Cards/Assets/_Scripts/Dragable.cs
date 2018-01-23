2959982de8187e8a07c5f9209336455e61e6c2e5
zoggerbaby <35176012+zoggerbaby@users.noreply.github.com>
2018-01-23 09:45:27 -0800

    Update
    stuff is starting to work

--- Cards/Assets/_Scripts/Dragable.cs
+++ Cards/Assets/_Scripts/Dragable.cs
@@ -4,47 +4,120 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Dragable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+public class Dragable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler {
 	public GameObject line;
 	public GameObject targetIcon;
+	public GameObject placeHolder;
+	[MaskEnum] public General.TargetTypes attackType;
 
 	private LineRenderer lr;
 	private Vector3 tmpVector;
+	private Image dragObject;
+	private List<RaycastResult> hits = new List<RaycastResult>();
 
 	void Start() {
 		lr = line.GetComponent<LineRenderer> ();
+		dragObject = GetComponent<Image> ();
+		StartCoroutine (AnimateLine ());
 	}
 
 	public void OnBeginDrag(PointerEventData evtData) {
-		targetIcon.SetActive (true);
-		line.SetActive (true);
-
-		tmpVector = transform.position;
-		tmpVector.z = 1;
-		lr.SetPosition (0, tmpVector);
+		dragObject.raycastTarget = false;
+		placeHolder.SetActive (true);
+		placeHolder.transform.SetSiblingIndex (dragObject.transform.GetSiblingIndex ());
+		dragObject.transform.SetParent (dragObject.transform.parent.parent);
 	}
 
 	public void OnDrag(PointerEventData evtData) {
 		tmpVector = Camera.main.ScreenToWorldPoint (evtData.position);
 		tmpVector.z = 1;
-		lr.SetPosition (1, tmpVector);
-		targetIcon.transform.position = tmpVector;
 
-		if (evtData.pointerCurrentRaycast.gameObject != null) {
-			print (evtData.pointerCurrentRaycast.gameObject.name);
-			if (evtData.pointerCurrentRaycast.gameObject.name == "PlayerImage") {
-				targetIcon.GetComponent<Image> ().color = Color.green;
-			} else {
-				targetIcon.GetComponent<Image> ().color = Color.red;
+		bool inParent = false;
+		EventSystem.current.RaycastAll (evtData, hits);
+		for (int i = 0; i < hits.Count; i++) {
+			if (hits[i].gameObject == placeHolder.transform.parent.gameObject) {
+				inParent = true;
+				break;
 			}
+		}
+
+		if (inParent) {
+			transform.position = tmpVector;
+
+			if (targetIcon.activeSelf) {
+				targetIcon.SetActive (false);
+				line.SetActive (false);
+			}
+			int SI = placeHolder.transform.parent.childCount;
+			for (int i = 0; i < placeHolder.transform.parent.childCount; i++) {
+				if (transform.position.x < placeHolder.transform.parent.GetChild (i).position.x) {
+					SI = i;
+					if (placeHolder.transform.GetSiblingIndex () < SI) {
+						SI--;
+					}
+					break;
+				}
+			}
+			placeHolder.transform.SetSiblingIndex (SI);
 		} else {
-			targetIcon.GetComponent<Image> ().color = Color.red;
+			if (attackType == General.TargetTypes.Player) {
+				if (!targetIcon.activeSelf) {
+					targetIcon.SetActive (true);
+					line.SetActive (true);
+
+					tmpVector = transform.position;
+					tmpVector.z = 1;
+					lr.SetPosition (0, tmpVector);
+				} else {
+					lr.SetPosition (1, tmpVector);
+					targetIcon.transform.position = tmpVector;
+					targetIcon.GetComponent<Image> ().color = Color.red;
+
+					GameObject go = evtData.pointerCurrentRaycast.gameObject;
+					if (go != null) {
+						Target t = go.GetComponent<Target> ();
+						if (t != null) {
+							if (t.targetType == attackType) {
+								targetIcon.GetComponent<Image> ().color = Color.green;
+							}
+						}
+					}
+				}
+			} else {
+				transform.position = tmpVector;
+			}
 		}
 	}
 
 	public void OnEndDrag(PointerEventData evtData) {
-		targetIcon.SetActive (false);
-		line.SetActive (false);
+		if (targetIcon.activeSelf) {
+			targetIcon.SetActive (false);
+			line.SetActive (false);
+		}
+		dragObject.raycastTarget = true;
+		dragObject.transform.SetParent (placeHolder.transform.parent);
+		dragObject.transform.SetSiblingIndex (placeHolder.transform.GetSiblingIndex ());
+		placeHolder.SetActive (false);
 	}
 
+	public void OnDrop(PointerEventData evtData) {
+
+	}
+
+	private IEnumerator AnimateLine() {
+		float framesPerSecond = 30;
+		Renderer r = line.GetComponent<Renderer> ();
+		Vector2 offset = Vector2.zero;
+
+		while (true) {
+			if (line.activeSelf) {
+				offset.x -= .05f;
+				if (offset.x < -1) {
+					offset.x += 1;
+				}
+				r.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+			}
+			yield return new WaitForSeconds(1f / framesPerSecond);
+		}	
+	}
 }
---
+++ Cards/Assets/_Scripts/General.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MaskEnum : PropertyAttribute
+{
+	public MaskEnum() { }
+}
+
+
+public class General : MonoBehaviour {
+
+	[System.Flags]
+	public enum TargetTypes {
+		Player 	= (1 << 0),
+		Card 	= (1 << 1),
+		Field 	= (1 << 2),
+	}
+
+}
