1b18b3154e45f86328b7ae9c2689fc97cd40c932
zoggerbaby <35176012+zoggerbaby@users.noreply.github.com>
2018-02-01 11:16:35 -0800

    Added WorldTree
    Beginning of a isometric hack&slash

---
+++ WorldTree/Assets/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+	public float playerMoveSpeed;
+	public float playerTurnSpeed;
+	public Transform playerTrans;
+
+	private Vector3 isoForward, isoRight;
+
+	void Start () {
+		isoForward = Camera.main.transform.forward.normalized;
+		isoForward.y = 0;
+		isoRight = Quaternion.Euler (new Vector3 (0, 90, 0)) * isoForward;
+		playerTrans.forward = (isoForward + isoRight).normalized;
+	}
+	
+	void Update () {
+		Vector3 moveDir = (isoRight * Input.GetAxisRaw ("Horizontal") + isoForward * Input.GetAxisRaw ("Vertical")).normalized;
+
+		if (moveDir != Vector3.zero) {
+			playerTrans.rotation = Quaternion.RotateTowards (playerTrans.rotation, Quaternion.LookRotation (moveDir), playerTurnSpeed);
+			transform.Translate (moveDir * playerMoveSpeed * Time.deltaTime);
+		}
+	}
+}
