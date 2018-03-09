a7ed84fcefc49047a0e63c49d9163f244544ce9d
zoggerbaby <35176012+zoggerbaby@users.noreply.github.com>
2018-03-09 11:53:00 -0800

    Added Golf
    Attempt at a golf game

---
+++ Golf/Assets/Shot.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Shot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public GameObject ball;
+	public Text helpText;
+	public float shotTime;
+
+	private float yStart, yEnd;
+	private float xLeft, xRight;
+	private float xAccStart, xAccEnd;
+	private Vector2 mPos;
+	private bool takingShot;
+	private Smack smack;
+
+	void Start () {
+		smack = ball.GetComponent<Smack> ();
+		RectTransform rt = GetComponent<RectTransform> ();
+
+		float yPos = Screen.height - transform.position.y;
+		yStart = yPos - rt.rect.size.y / 2 + 5;
+		yEnd = yPos + rt.rect.size.y / 2;
+
+		float xPos = Screen.width - transform.position.x;
+		xLeft = xPos - rt.rect.size.x / 2;
+		xRight = xPos + rt.rect.size.x / 2;
+
+		takingShot = false;
+	}
+	
+	void Update () {
+		mPos = Input.mousePosition;
+		if (Input.GetMouseButton (0)) {
+			if (mPos.x > xLeft && mPos.x < xRight && mPos.y < yStart) {
+				takingShot = true;
+			}
+		} else {
+			takingShot = false;
+		}
+	}
+
+	public void OnPointerEnter (PointerEventData eventData) 
+	{
+		if (takingShot) {
+			xAccStart = eventData.position.x;
+			shotTime = Time.time;
+		}
+	}
+
+	public void OnPointerExit (PointerEventData eventData) 
+	{
+		if (takingShot && Input.mousePosition.y > yEnd) {
+			shotTime = Time.time - shotTime;
+			xAccEnd = eventData.position.x;
+			takingShot = false;
+			smack.TakeShot (250 - shotTime * 300, xAccStart - xAccEnd);
+		}
+	}
+
+}
---
+++ Golf/Assets/Smack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Smack : MonoBehaviour {
+
+	public Text helpText;
+
+	private Rigidbody rb;
+	private ConstantForce cf;
+	private bool hitGround;
+	private Vector3 camOffset;
+	private Vector3 ballStart;
+	private bool inFlight = false;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		cf = GetComponent<ConstantForce> ();
+		camOffset = Camera.main.transform.position;
+	}
+	
+	void Update () {
+
+		if (Input.GetMouseButtonDown (1)) {
+			transform.position = new Vector3 (0, .5f, 0);
+		}
+
+		if (inFlight) {
+			if (rb.velocity.magnitude < .3f) {
+				rb.velocity = Vector3.zero;
+				helpText.text = "Ball few: " + Vector3.Distance (ballStart, transform.position).ToString ();
+				inFlight = false;
+			} else {
+				if (hitGround) {
+					cf.force = Vector3.zero;
+				}
+			}
+
+			Camera.main.transform.position = transform.position + camOffset;
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		hitGround = true;
+		rb.angularDrag = 5f;
+	}
+
+	public void TakeShot(float power, float accuracy) {
+		ballStart = transform.position;
+		rb.angularDrag = .05f;
+		rb.velocity = (Vector3.forward * power + Vector3.up * 35);
+		cf.force = new Vector3(accuracy*.2f, 0, 0);
+		hitGround = false;
+		inFlight = true;
+	}
+}
